b6c44b3dcee725ea3bbb15cf9037c0f60a5f7f28
vacca_j <jordan.vacca@epitech.eu>
2018-03-17 15:40:03 +0100

    [+] add some features without const

--- Mike_iOS/Controller/profileVC.swift
+++ Mike_iOS/Controller/profileVC.swift
@@ -10,26 +10,23 @@ import UIKit
 
 class profileVC: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet weak var profilImg: UIImageView!
     
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        setupProfil()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
+
+
+    private func setupProfil()
+    {
+        self.profilImg.layer.cornerRadius = self.profilImg.frame.size.width / 2
+        self.profilImg.clipsToBounds = true
+        self.profilImg.layer.borderWidth = 2
+        self.profilImg.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
 
 }
