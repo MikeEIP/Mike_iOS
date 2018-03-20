9561b23db57bcbfc604ff7ec60b80bf036ae7ea7
vacca_j <jordan.vacca@epitech.eu>
2018-03-20 15:29:50 +0100

    [~] Merge Correcting

---
+++ Mike_iOS/View/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  Mike_iOS
+//
+//  Created by Jordan Vacca on 20/03/2018.
+//  Copyright © 2018 JeremyP. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+    
+    override func awakeFromNib() {
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+}
