a05a6316448cea2e60662f324b571bcda43c5e3c
vacca_j <jordan.vacca@epitech.eu>
2018-03-21 17:18:14 +0100

    [+] Add article views & add graph in training views

--- Mike_iOS/Controller/TrainingVC.swift
+++ Mike_iOS/Controller/TrainingVC.swift
@@ -11,20 +11,107 @@ import UIKit
 class TrainingVC: UIViewController {
 
     @IBOutlet weak var trainingBtn: UIButton!
+
     
-    override func viewDidLoad()
+    @objc private func handleEnterForeground()
     {
+        animatePulsatingLayer()
+    }
+    
+    var shapeLayer: CAShapeLayer!
+    var pulsatingLayer: CAShapeLayer!
+    
+    let percentageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "100%"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.textColor = .white
+        return label
+    }()
+    
+    
+    private func createCircleShapeLayer(strokeColor: UIColor, fillColor: UIColor) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        layer.path = circularPath.cgPath
+        layer.strokeColor = strokeColor.cgColor
+        layer.lineWidth = 20
+        layer.fillColor = fillColor.cgColor
+        layer.lineCap = kCALineCapRound
+        layer.position = view.center
+        return layer
+    }
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
         trainingBtn.layer.cornerRadius = trainingBtn.frame.size.height / 2
         trainingBtn.clipsToBounds = true
-       
         
+        
+        //setupNotificationObservers()
+        
+        setupCircleLayers()
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        
+        setupPercentageLabel()
+    }
+    
+    private func setupPercentageLabel() {
+        view.addSubview(percentageLabel)
+        percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        percentageLabel.center = view.center
+    }
+    
+    private func setupCircleLayers() {
+        pulsatingLayer = createCircleShapeLayer(strokeColor: .clear, fillColor: UIColor.pulsatingFillColor)
+        view.layer.addSublayer(pulsatingLayer)
+        animatePulsatingLayer()
+        
+        let trackLayer = createCircleShapeLayer(strokeColor: .trackStrokeColor, fillColor: UIColor.clear)
+        view.layer.addSublayer(trackLayer)
+        
+        shapeLayer = createCircleShapeLayer(strokeColor: .outlineStrokeColor, fillColor: .clear)
+        
+        shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
+        shapeLayer.strokeEnd = 0
+        view.layer.addSublayer(shapeLayer)
+    }
+    
+    private func animatePulsatingLayer() {
+        let animation = CABasicAnimation(keyPath: "transform.scale")
+        
+        animation.toValue = 1.3
+        animation.duration = 0.8
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        animation.autoreverses = true
+        animation.repeatCount = Float.infinity
+        
+        pulsatingLayer.add(animation, forKey: "pulsing")
     }
     
     
-    @IBAction func trainingBtnPressed(_ sender: Any)
-    {
+    
+    fileprivate func animateCircle() {
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
         
+        basicAnimation.toValue = 1
+        
+        basicAnimation.duration = 2
+        
+        basicAnimation.fillMode = kCAFillModeForwards
+        basicAnimation.isRemovedOnCompletion = false
+        
+        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
+    }
+    
+    @objc private func handleTap() {
+        print("Attempting to animate stroke")
+        
+     //   beginDownloadingFile()
+        
+            animateCircle()
     }
     
 }
